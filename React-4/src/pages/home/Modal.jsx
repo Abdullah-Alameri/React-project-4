08a7f58105c73caeb2e65156ed9ad84b1c2b0575
Abdullah <liten30880@wwgoc.com>
2023-08-26 17:55:12 +0200

    New Modal

--- React-4/src/pages/home/Modal.jsx
+++ React-4/src/pages/home/Modal.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactLoading from "react-loading";
 import Modal from "../../shared/Modal";
 
-
 const HomeModal = ({
   closeModal,
   titleInput,
@@ -13,36 +12,108 @@ const HomeModal = ({
   subTask,
   array,
   showLoading,
-
 }) => {
   return (
+    //   <Modal closeModal={closeModal}>
+    //   <div
+    //     className="model-content"
+    //     style={{ textAlign: "left", height: "100%" }}
+    //   >
+    //     <input
+    //       onChange={(eo) => {
+    //         titleInput(eo);
+    //       }}
+    //       required
+    //       placeholder="Add title:"
+    //       type="text"
+    //       value={taskTitle}
+    //     />
+
+    //     <div>
+    //       <input
+    //         onChange={(eo) => {
+    //           datailsInput(eo);
+    //         }}
+    //         required
+    //         placeholder="Details:"
+    //         type="text"
+    //         value={subTask}
+    //       />
+
+    //       <button
+    //         onClick={(eo) => {
+    //           addBTN(eo);
+    //         }}
+    //       >
+    //         Add
+    //       </button>
+    //     </div>
+
+    //     <ul>
+    //       {array.map((item) => (
+    //         <li key={item}>{item}</li>
+    //       ))}
+    //     </ul>
+
+    //     <button
+    //       style={{ marginBottom: "20px" }}
+    //       onClick={(eo) => {
+    //         submitBTN(eo);
+    //       }}
+    //     >
+    //       {showLoading ? (
+    //         <ReactLoading
+    //           type={"spin"}
+    //           color={"white"}
+    //           height={20}
+    //           width={20}
+    //         />
+    //       ) : (
+    //         "Submit"
+    //       )}
+    //     </button>
+    //   </div>
+    // </Modal>
+
     <Modal closeModal={closeModal}>
       <div
-        className="model-content"
+        className="model-content form__group field"
         style={{ textAlign: "left", height: "100%" }}
       >
-        <input
-          onChange={(eo) => {
-            titleInput(eo);
-          }}
-          required
-          placeholder="Add title:"
-          type="text"
-          value={taskTitle}
-        />
+        <div className="form__group field ">
+          <input
+            onChange={(eo) => {
+              titleInput(eo);
+            }}
+            requi
+            required
+            placeholder="Title"
+            className="form__field"
+            type="text"
+            value={taskTitle}
+          />
+          <label className="form__label" htmlFor="name">
+            Title
+          </label>
+        </div>
 
-        <div>
+        <div className="form__group field flex">
           <input
             onChange={(eo) => {
               datailsInput(eo);
             }}
             required
-            placeholder="Details:"
+            placeholder="Details"
+            className="form__field"
             type="text"
             value={subTask}
           />
-
+          <label className="form__label" htmlFor="name">
+            Details
+          </label>
           <button
+            style={{ marginLeft: "10px", marginTop: "px" }}
+            className="modal__btn"
             onClick={(eo) => {
               addBTN(eo);
             }}
@@ -58,7 +129,8 @@ const HomeModal = ({
         </ul>
 
         <button
-          style={{ marginBottom: "20px" }}
+          className="modal__btn"
+          style={{ marginBottom: "15px" }}
           onClick={(eo) => {
             submitBTN(eo);
           }}
@@ -67,8 +139,8 @@ const HomeModal = ({
             <ReactLoading
               type={"spin"}
               color={"white"}
-              height={20}
-              width={20}
+              height={25}
+              width={25}
             />
           ) : (
             "Submit"
